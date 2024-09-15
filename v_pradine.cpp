99f140477bbcb489e4d5dc87faab561c82cc002e
aemilija <abromaityteemilija@gmail.com>
2024-09-15 19:57:59 +0300

    pridedamos nd vidurkio ir galutinio pazymio skaiciavimo funkcijos

--- v_pradine.cpp
+++ v_pradine.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+
 using namespace std;
 
 struct Stud { //sukuriama studento struktura, kurioje saugoma vardas, pavarde, namu darbu (2) ir egzamino pazymiai
@@ -6,4 +7,15 @@ struct Stud { //sukuriama studento struktura, kurioje saugoma vardas, pavarde, n
     string pavarde;
     int nd[2]; //kadangi pirma nepasakyta, kad nezinome nd skaiciaus, tariame kad ju yra 2
     int egz;
+};
+
+//funkcija, kuri suskaiciuoja namu darbu vidurki
+double ndVidurkis(const Stud &student) { 
+    return (student.nd[0] + student.nd[1]) / 2.0; 
+};
+
+//funkcija, kuri suskaiciuoja galutini rezultata, naudojantis formule 0.4*ndAvg+0.6+egz
+double galutinis(const Stud &student) {
+    double ndVid = ndVidurkis(student);
+    return 0.4 * ndVid + 0.6 * student.egz;
 };
