bb02bb0f742082a997e921d87e8e621daab8fd24
aemilija <abromaityteemilija@gmail.com>
2024-09-15 17:56:27 +0300

    sukuriama studento struktūra

--- v_pradine.cpp
+++ v_pradine.cpp
@@ -1,1 +1,9 @@
+#include <iostream>
+using namespace std;
 
+struct Stud { //sukuriama studento struktura, kurioje saugoma vardas, pavarde, namu darbu (2) ir egzamino pazymiai
+    string vardas;
+    string pavarde;
+    int nd[2]; //kadangi pirma nepasakyta, kad nezinome nd skaiciaus, tariame kad ju yra 2
+    int egz;
+};
