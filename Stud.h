641818aa92e24026aac2587b36f55d00ed4e3900
aemilija <abromaityteemilija@gmail.com>
2024-10-22 19:01:59 +0300

    vector pakeista i list

--- Stud.h
+++ Stud.h
@@ -3,19 +3,20 @@
 struct Stud { //sukuriama studento struktura, kurioje saugoma vardas, pavarde, namu darbu (2) ir egzamino pazymiai
     string vardas;
     string pavarde;
-    vector<int> nd; //nd saugome vector tipo konteineryje
+    vector<int> nd; 
     int egz;
 };
 
 double ndVidurkis(const Stud &student);
 double ndMediana(const Stud &student);
 double galutinis(const Stud &student, char pasirinkimas);
-void ivestiDuomenisRanka(vector<Stud> &student, int ndSkaicius);
-void atsitiktiniaiPazymiai(vector<Stud> &student, double ndSkaicius);
-void nuskaitytiFaila(vector<Stud> &student, string failoPav);
-void spausdinti(vector<Stud> &student, char pasirinkimas);
-void isvestiFaila(vector<Stud> student, char pasirinkimas, string failoPav);
+void ivestiDuomenisRanka(list<Stud> &student, int ndSkaicius);
+void atsitiktiniaiPazymiai(list<Stud> &student, double ndSkaicius);
+void nuskaitytiFaila(list<Stud> &student, string failoPav);
+void spausdinti(list<Stud> &student, char pasirinkimas);
+void spausdintiSuAdresu(list<Stud> &student, char pasirinkimas);
+void isvestiFaila(list<Stud> student, char pasirinkimas, string failoPav);
 void generuotiFailus(string failoPav, int studSk);
-void paskirtytiStud(vector<Stud> &studentai, vector<Stud> &saunuoliai, vector<Stud> &nevykeliai, char pasirinkimas);
+void paskirtytiStud(list<Stud> &studentai, list<Stud> &saunuoliai, list<Stud> &nevykeliai, char pasirinkimas);
 std::chrono::steady_clock::time_point dabLaikas();
 void valymas(Stud & student);
