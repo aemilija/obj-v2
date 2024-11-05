9ad5931b8bc12f41b3e6a34d61a0f6ba90e29b74
aemilija <abromaityteemilija@gmail.com>
2024-11-05 20:51:46 +0200

    Add files via upload

---
+++ strategija_3/header.h
@@ -0,0 +1,16 @@
+#ifndef STUDENT_H
+#define STUDENT_H
+
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <algorithm>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <limits>
+#include <chrono>
+
+using namespace std;
+
+#endif
