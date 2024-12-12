fd71feebd150efda91332dad6843f075b9b0399e
aemilija <abromaityteemilija@gmail.com>
2024-12-12 21:38:42 +0200

    komentarai

--- class/Stud.h
+++ class/Stud.h
@@ -1,4 +1,10 @@
 #include "header.h"
+
+/**
+ * Abstrakti klase Zmogus, kuri apibrezia zmogaus objekto savybes
+ * Turi du parametrus - varda ir pavarde, default ir parametrizuotas konstruktoriu, destruktoriu, getterius ir setterius.
+ */
+
 class Zmogus {
     protected:
     string vardas;
@@ -22,18 +28,40 @@ class Zmogus {
 
 inline Zmogus::~Zmogus() {}
 
+/**
+ * Derived klase Stud, kuri apibrezia studento objekto savybes
+ * Paveldi viska is klases Zmogus ir turi savo du parametus - namu darbus ir egzamina. Turi savo default ir parametrizuotus konstruktorius, destruktorius, getterius ir setterius.
+ * Apibrezti kopijavimo ir kopijavimo priskyrimo konstruktoriai
+ */
+
 class Stud : public Zmogus {
     private:
     vector<int> nd; 
     int egz;
 
     public:
-    //konstruktorius
+
+    /**
+     * @brief Konstruktorius, kuris vardui pavardei priskiria tuscia string, namu darbai yra tuscias vektrius ir egzamino pazymys lygus 0
+     */
+
     Stud() : Zmogus("", ""), nd{}, egz(0) {}
+
+    /**
+     * Konstruktorius, kuris suteikia visiems klases parametrams reiksmes
+     * @param v Studento vardas
+     * @param p Studento pavarde
+     * @param n Studento namu darbu pazymiai
+     * @param e Studento egzamino pazymys
+     */
+
     Stud(const string& v, const string& p, const vector<int> n, int e) : Zmogus(v, p), nd(n), egz(e) {}
 
-    //destruktorius
-    ~Stud() override {
+    /**
+     * @brief Destruktorius, kuris isvalo visus duomenis
+     */
+
+    ~Stud() {
         vardas.clear();
         pavarde.clear();
         nd.clear();
@@ -42,10 +70,19 @@ class Stud : public Zmogus {
         // cout << "destrukorius ivykdytas" << endl;
     }
 
-    //kopijavimo konstruktorius
+    /**
+     * @brief Kopijavimo konstruktorius, kuris sukuria kopija is kito studento objekto
+     * @param kitas Studentas, kurio duomenys bus nukopijuoti
+     */
+
     Stud(const Stud &kitas) : Zmogus(kitas.vardas, kitas.pavarde), nd(kitas.nd), egz(kitas.egz) {}
     
-    //kopijavimo priskyrimo operatorius
+    /**
+     * @brief Kopijavimo priskyrimo operatorius, kuris priskiria vieno studento duomenis kitam
+     * @param kitas Studentas, kurio duomenys bus priskirti kitam
+     * @return Grazina nuoroda i dabartini objekta
+     */
+
     Stud& operator = (const Stud &kitas) {
         if (this != &kitas) {
             vardas = kitas.vardas;
@@ -56,7 +93,13 @@ class Stud : public Zmogus {
         return *this;
     } 
 
-    //ivesties operatorius
+    /**
+     * @brief Ivesties operatorius, kuriuo ivedami studento duomenys
+     * @param is Ivesties srautas
+     * @param stud Studentas, kurio duomenys ivedami
+     * @return Ivesties srautas
+     */
+
     friend istream& operator >> (istream &is, Stud &stud) {
         cout << "Iveskite studento varda pavarde: ";
         is >> stud.vardas >> stud.pavarde;
@@ -80,7 +123,13 @@ class Stud : public Zmogus {
 
     }
 
-    //isvesties operatorius
+    /**
+     * @brief Isvesties operatorius, kuris isveda studento duomenis
+     * @param out Isvesties srautas
+     * @param stud Studentas, kurio duomenys bus isvedami
+     * @return Isvesties srautas
+     */
+
     friend ostream& operator << (ostream &out, const Stud &stud) {
         out << "Vardas: " << stud.getVardas() << endl;
         out << "Pavarde: " << stud.getPavarde() << endl;
@@ -94,7 +143,24 @@ class Stud : public Zmogus {
         return out;
     }
 
-    //getteriai, setteriai
+    //getteriaim, setteriai
+    
+    string getVardas() const {
+        return vardas;
+    }
+
+    void setVardas (const string &v) {
+        vardas = v;
+    }
+
+    string getPavarde() const {
+        return pavarde;
+    }
+
+    void setPavarde(const string &p) {
+        pavarde = p;
+    }
+
     vector<int> getNd() const {
         return nd;
     }
@@ -123,7 +189,7 @@ class Stud : public Zmogus {
 double ndVidurkis(const Stud &student);
 double ndMediana(const Stud &student);
 double galutinis(const Stud &student, char pasirinkimas);
-void ivestiDuomenisRanka(vector<Stud> &student, int ndSkaicius);
+void ivestiDuomenisRanka(vector<Stud> &student);
 void atsitiktiniaiPazymiai(vector<Stud> &student, double ndSkaicius);
 void nuskaitytiFaila(vector<Stud> &student, string failoPav);
 void spausdinti(vector<Stud>& studentai, char pasirinkimas);
